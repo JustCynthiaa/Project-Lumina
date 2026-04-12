e05064bb8666073c5034bd353a95708122a5388b
MrEarv <earvinarizmendi@gmail.com>
2026-04-11 19:36:14 -0600

    hola, es un prueba mia

--- app.py
+++ app.py
@@ -5,6 +5,6 @@ app = Flask(__name__)
 @app.route('/')
 def home():
     return "<h1>¡Hola desde Flask!</h1><p>Texto de prueba</p>"
-
+# comentario de Earvin como prueba de commit
 if __name__ == '__main__':
     app.run(debug=True)
