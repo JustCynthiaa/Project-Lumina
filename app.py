3639b356bd74c733ceb03969ad020e8e5a00c592
MrEarv <earvinarizmendi@gmail.com>
2026-05-04 19:20:37 -0600

    añadi la interfaz que habia hecho

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "<h1>¡Hola desde Flask!</h1><p>Texto de prueba</p>"
+    #return "<h1>¡Hola desde Flask!</h1><p>Texto de prueba</p>"
+    return render_template('index.html')
 # comentario de Earvin como prueba de commit
 if __name__ == '__main__':
     app.run(debug=True)
