e713e8fb2ecc872d8c81c0bbdac1273691080d6a
MrEarv <earvinarizmendi@gmail.com>
2026-05-04 22:03:43 -0600

    base y librerias para la bd creada

--- app.py
+++ app.py
@@ -1,6 +1,42 @@
+import os
 from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
+from dotenv import load_dotenv
+
+load_dotenv()
 
 app = Flask(__name__)
+
+usuario = os.getenv('DB_USER')
+contraseña = os.getenv('DB_PASSWORD')
+host = os.getenv('DB_HOST')
+nombre_bd = os.getenv('DB_NAME')
+
+app.config['SQLALCHEMY_DATABASE_URI'] = f'mysql+pymysql://{usuario}:{contraseña}@{host}/{nombre_bd}'
+db = SQLAlchemy(app)
+
+class Usuario(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    nombre = db.Column(db.String(50), nullable=False)
+    password = db.Column(db.String(100), nullable=False)
+
+
+# Nota importante: cualquier cambio en el modelo de datos (clases) requiere eliminar la base de datos y crearla nuevamente para reflejar los cambios.
+# Esto se hace fácilmente eliminando la base de datos desde phpMyAdmin y luego ejecutando el código nuevamente para que se cree una nueva base de datos con las tablas actualizadas.
+# python app.py
+
+with app.app_context():
+    db.create_all()
+
+    #usuario admin por defecto
+    if not Usuario.query.filter_by(nombre='admin').first():
+        admin = Usuario(nombre='admin', password='admin123')
+        db.session.add(admin)
+        db.session.commit()
+        print("Usuario admin creado con éxito.")
+    else:
+        print("Usuario admin ya existe. Continuando...")
+
 
 @app.route('/')
 def home():
